2ad6bbe494863ea2ef465b4abbcf47551e891cfb
BuildTools <jonas.boeschemeier@gmail.com>
2016-03-14 17:28:27 +0100

    merge

--- src/de/titanium/enterprise/View/DefenseGame/DefenseMenu.java
+++ src/de/titanium/enterprise/View/DefenseGame/DefenseMenu.java
@@ -17,10 +17,12 @@ import java.util.concurrent.SynchronousQueue;
  */
 public class DefenseMenu extends MenuView implements GameComponent {
 
+    private final Random random = new Random();
     private List<Rectangle[]> rectangles = new ArrayList<>();
 
     private int space = 50;
     private int tick = 0;
+    private int speed = 10;
 
     public DefenseMenu() {
         Enterprise.getGame().addComponent(this);
@@ -44,9 +46,11 @@ public class DefenseMenu extends MenuView implements GameComponent {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.setColor(Color.BLACK);
-        for(Rectangle[] set : this.rectangles) {
-            for(Rectangle rectangle : set) {
+        g.setColor(Color.BLUE);
+        Iterator<Rectangle[]> iterator = this.rectangles.iterator();
+
+        while(iterator.hasNext()){
+            for(Rectangle rectangle : iterator.next()) {
                 g.fillRect(
                         (int) rectangle.getX(),
                         (int) rectangle.getY(),
@@ -61,12 +65,11 @@ public class DefenseMenu extends MenuView implements GameComponent {
     @Override
     public void update(double deltaTime, int tick) {
 
-        this.tick++;
-
         //Alle 5 Sekunden wird der Abstand zwischen den Beiden Modulen um 1 verringert.
-        if(this.tick % 100 == 0) {
-            //this.space--;
+        if(this.tick % 500 == 0) {
+            this.speed += 2;
         }
+        this.tick++;
 
         //Updaten aller Module
         Iterator<Rectangle[]> rectangles = this.rectangles.iterator();
@@ -78,15 +81,14 @@ public class DefenseMenu extends MenuView implements GameComponent {
 
             //Falls das letzte Elemente sich nicht mehr im Screen befindet wird es entfernt und ein neues wird hinzugefügt.
 
-            int x = (int) Math.ceil((deltaTime / 5) * 2) + 1;
              if(rec[rec.length-1].x + rec[rec.length-1].getWidth() < 0) {
                 rectangles.remove();
 
                 Rectangle[] last = this.rectangles.get(this.rectangles.size() - 1);
-                tmp.add(DefenseModules.STAIR.getRectangles(1080 - x, this.space, (int) last[last.length - 2].getHeight()));
+                tmp.add(DefenseModules.STAIR.getRectangles(1080 - speed, this.space, (int) last[last.length - 2].getHeight()));
             } else {
                 for (Rectangle rectangle : rec) {
-                    rectangle.x -= x;
+                    rectangle.x -= speed / 2;
                 }
             }
 
