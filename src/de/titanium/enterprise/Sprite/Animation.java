62f9ed212b11826c5484f3128eafa1e7b30b1323
sweetcode <jan@krueger-jan.de>
2016-03-16 20:38:57 +0100

    Background

---
+++ src/de/titanium/enterprise/Sprite/Animation.java
@@ -0,0 +1,15 @@
+package de.titanium.enterprise.Sprite;
+
+/**
+ * Created by Yonas on 16.03.2016.
+ */
+public class Animation {
+
+    private final Texture[] textures;
+    private int currentFrame = 0;
+
+    public Animation(Texture[] textures) {
+        this.textures = textures;
+    }
+
+}
