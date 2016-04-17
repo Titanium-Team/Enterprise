1f8705def4e8670b454164a38119d642b9ee889f
sweetcode <jan@krueger-jan.de>
2016-04-17 12:24:39 +0200

    removed version.json

--- src/de/titanium/enterprise/Game.java
+++ src/de/titanium/enterprise/Game.java
@@ -23,13 +23,14 @@ public class Game {
             Game.path.mkdirs();
         }
 
+        // Die Daten von GitHub laden
+        JsonObject githubData = Game.gson.fromJson(new String(IOUtils.toString(new URL("https://api.github.com/repos/Titanium-Team/Enterprise/releases/latest"), StandardCharsets.UTF_8)), JsonObject.class);
+        String latestVersion = githubData.get("tag_name").getAsString();
+
         Game.writeVersionToFile(null);
 
         // Die aktuelle Version auslesen
         String readableVersion = Game.getCurrentVersion();
-
-        JsonObject githubData = Game.gson.fromJson(new String(IOUtils.toString(new URL("https://api.github.com/repos/Titanium-Team/Enterprise/releases/latest"), StandardCharsets.UTF_8)), JsonObject.class);
-        String latestVersion = githubData.get("tag_name").getAsString();
 
         if(readableVersion.equalsIgnoreCase("vunknown")) {
 
@@ -47,6 +48,9 @@ public class Game {
                 System.out.println("Du hast aktuelle die Version " + readableVersion + " installiert, es steht aber bereits Version " + latestVersion + " bereit.");
                 System.out.println("Die neue Version kannst du hier downloaden.");
                 System.out.println(githubData.get("assets").getAsJsonArray().get(0).getAsJsonObject().get("browser_download_url").getAsString());
+                System.out.println("");
+                System.out.println("1. Ersetze die aktuelle \"Enterprise.jar\" mit der neusten \"Enterprise.jar\".");
+                System.out.println("2. Entferne die \"version.json\" Datei.");
                 System.out.println("------------------------------------------------------");
             }
 
@@ -80,17 +84,16 @@ public class Game {
 
         // Version
         File file = new File(Game.path.getPath() + "/version.json");
-
-        JsonObject version = new JsonObject();
-
         if(!(file.exists())) {
 
             file.createNewFile();
+            JsonObject version = new JsonObject();
             version.addProperty("version", "vunknown");
             Files.write(Paths.get(file.getPath()), Game.gson.toJson(version).getBytes(StandardCharsets.UTF_8));
 
         } else if(!(value == null)) {
 
+            JsonObject version = new JsonObject();
             version.addProperty("version", value);
             Files.write(Paths.get(file.getPath()), Game.gson.toJson(version).getBytes(StandardCharsets.UTF_8));
 
